f1e801c6cd882a7ac0257a0012418f06ab0ad7de
Andrew Arrow <andrew@0x7a69.net>
2016-03-12 06:29:32 -0800

    flow

--- paradice.go
+++ paradice.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"paradice/server"
 )
 
 func main() {
-	server.Run()
+	url := fmt.Sprintf("localhost:%d", 2121) // change to 21 in production
+	var listener net.Listener
+	listener, err := net.Listen("tcp", url)
+
+	if err != nil {
+		fmt.Println("cannot listen on: ", url)
+		return
+	}
+	fmt.Println("listening on: ", url)
+
+	for {
+		holder := server.ConnectionHolder{}
+		holder.Path = "/"
+		holder.TheConnection, err = listener.Accept()
+		if err != nil {
+			fmt.Println("listening error ", err)
+			break
+		}
+
+		fmt.Println("Got client on: ", holder.TheConnection.RemoteAddr().String())
+
+		go server.HandleCommands(&holder)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,4 +1,18 @@
 package server
 
-func Run() {
+import (
+	"net"
+	"sync"
+)
+
+type ConnectionHolder struct {
+	TheConnection net.Conn
+	PassiveConn   *net.TCPConn
+	Waiter        sync.WaitGroup
+	User          string
+	HomeDir       string
+	Path          string
+}
+
+func HandleCommands(holder *ConnectionHolder) {
 }
