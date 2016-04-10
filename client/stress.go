aa215e8c472ec161537f541abb35617cf6b1626f
Andrew Arrow <andrew@0x7a69.net>
2016-04-10 08:39:15 -0700

    start of stress test

---
+++ client/stress.go
@@ -0,0 +1,7 @@
+package client
+
+import "fmt"
+
+func StressTest() {
+	fmt.Println("test")
+}
--- paradise.go
+++ paradise.go
@@ -1,7 +1,12 @@
 package main
 
 import "paradise/server"
+import "paradise/client"
+import "os"
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "test" {
+		go client.StressTest()
+	}
 	server.Start()
 }
