5ef78bb6716b6e89ec5d1a88a79e06595b3adac7
Andrew Arrow <andrew@0x7a69.net>
2016-04-10 13:09:39 -0700

    settings

---
+++ server/settings.go
@@ -0,0 +1,28 @@
+package server
+
+import "github.com/naoina/toml"
+import "os"
+import "io/ioutil"
+
+type ParadiseSettings struct {
+	Host           string
+	Port           int
+	MaxConnections int
+}
+
+func ReadSettings() ParadiseSettings {
+	f, err := os.Open("conf/settings.toml")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	buf, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	var config ParadiseSettings
+	if err := toml.Unmarshal(buf, &config); err != nil {
+		panic(err)
+	}
+	return config
+}
