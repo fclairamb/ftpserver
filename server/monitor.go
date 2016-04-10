5944d8f81167e9222fb74e9342e19af6c1735978
Andrew Arrow <andrew@0x7a69.net>
2016-04-10 09:35:40 -0700

    monitor output

--- server/monitor.go
+++ server/monitor.go
@@ -5,7 +5,20 @@ import "time"
 
 func Monitor() {
 	for {
-		fmt.Println(" * MONITOR *")
-		time.Sleep(1 * time.Second)
+		fmt.Println("9 clients, 4 passive, Up for 12:33:12.34")
+		fmt.Println("   PFC106 00:17:34")
+		fmt.Println("   PFC101 00:09:04")
+		fmt.Println("   PFC109 00:07:04")
+		fmt.Println("     PFC109p 03:33")
+		fmt.Println("     PFC109p 01:33")
+		fmt.Println("     PFC109p 00:33")
+		fmt.Println("   PFC116 00:06:04")
+		fmt.Println("   PFC126 00:05:30")
+		fmt.Println("   PFC104 00:04:30")
+		fmt.Println("   PFC206 00:03:30")
+		fmt.Println("     PFC206p 00:02:30")
+		fmt.Println("   PFC306 00:02:30")
+		fmt.Println("   PFC301 00:01:30")
+		time.Sleep(5 * time.Second)
 	}
 }
