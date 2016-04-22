4f52ec1a0a57f8c35c8ffeb9c25aeff7a58d0831
Andrew Arrow <andrew@0x7a69.net>
2016-04-22 10:04:10 -0700

    use_auth

--- server/auth.go
+++ server/auth.go
@@ -1,13 +1,13 @@
 package server
 
 func (p *Paradise) HandleUser() {
-	//fmt.Println(p.ip, p.command, p.param)
 	p.user = p.param
 	p.writeMessage(331, "User name ok, password required")
 }
 
 func (p *Paradise) HandlePass() {
-	if true { // change to your auth logic, think about using https://developer.bitium.com
+	// think about using https://developer.bitium.com
+	if AuthManager.CheckUser(p.user, p.param) {
 		p.writeMessage(230, "Password ok, continue")
 	} else {
 		p.writeMessage(530, "Incorrect password, not logged in")
