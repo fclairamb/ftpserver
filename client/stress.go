17e8a8bde721192c38738f9235a0fce43081447c
Andrew Arrow <andrew@0x7a69.net>
2016-04-22 08:23:42 -0700

    stress

--- client/stress.go
+++ client/stress.go
@@ -2,17 +2,27 @@ package client
 
 import "time"
 
-func StressTest() {
-	time.Sleep(1 * time.Second)
+func run() {
 	c := NewClient(1)
 	c.Connect()
 	c.List()
-	c.Stor(1024)
-	time.Sleep(1 * time.Second)
-	c.List()
-	time.Sleep(1 * time.Second)
-	c.Stor(1024 * 20)
-	time.Sleep(1 * time.Second)
-	time.Sleep(1 * time.Second)
-	c.List()
+	c.Stor(1024 * 1024 * 200)
+	c.Stor(1024 * 1024 * 200)
+	c.Stor(1024 * 1024 * 200)
+	c.Stor(1024 * 1024 * 200)
+}
+
+func StressTest() {
+	for {
+		time.Sleep(1 * time.Second)
+		go run()
+		c := NewClient(1)
+		c.Connect()
+		c.List()
+		c.Stor(1024 * 1024 * 200)
+		c.List()
+		c.Stor(1024 * 1024 * 400)
+		c.List()
+		go run()
+	}
 }
