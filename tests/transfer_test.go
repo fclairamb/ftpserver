c3459268b21faae9078305084c1d7c10392fee10
Florent Clairambault <florent@clairambault.fr>
2017-04-08 22:53:52 +0200

    Integrating gocyclo into the travis build checks

--- tests/transfer_test.go
+++ tests/transfer_test.go
@@ -12,6 +12,102 @@ import (
 	"testing"
 )
 
+func createTemporaryFile(t *testing.T, targetSize int) *os.File {
+	var file *os.File
+	var fileErr error
+
+	if file, fileErr = ioutil.TempFile("", "ftpserver"); fileErr != nil {
+		t.Fatal("Temporary creation error:", fileErr)
+		return nil
+	}
+
+	src := rand.New(rand.NewSource(0))
+	if _, err := io.CopyN(file, src, int64(targetSize)); err != nil {
+		t.Fatal("Couldn't copy:", err)
+		return nil
+	}
+	return file
+}
+
+func hashFile(t *testing.T, file *os.File) string {
+	if _, err := file.Seek(0, 0); err != nil {
+		t.Fatal("Couldn't seek:", err)
+		return nil
+	}
+	hashser := sha256.New()
+	if _, err := io.Copy(hashser, file); err != nil {
+		t.Fatal("Couldn't hashUpload:", err)
+	}
+
+	hash := hex.EncodeToString(hashser.Sum(nil))
+	if _, err := file.Seek(0, 0); err != nil {
+		t.Fatal("Couldn't seek:", err)
+	}
+	return hash
+}
+
+func ftpUpload(t *testing.T, ftp *goftp.FTP, file *os.File, filename string) {
+	if _, err := file.Seek(0, 0); err != nil {
+		t.Fatal("Couldn't seek:", err)
+	}
+	if err := ftp.Stor(filename+".tmp", file); err != nil {
+		t.Fatal("Couldn't ftpUpload bin:", err)
+	}
+
+	if err := ftp.Rename(filename+".tmp", filename); err != nil {
+		t.Fatal("Can't rename file:", err)
+	}
+
+	if _S, err := ftp.Size("file1.bin"); err != nil {
+		t.Fatal("Couldn't get the size of file1.bin:", err)
+	}
+
+	if stats, err := ftp.Stat("file1.bin"); err != nil {
+		// That's acceptable for now
+		t.Log("Couldn't stat file:", err)
+	} else {
+		found := false
+		for _, l := range stats {
+			if strings.HasSuffix(l, "file1.bin") {
+				found = true
+			}
+		}
+		if !found {
+			t.Fatal("STAT: Couldn't find file !")
+		}
+	}
+}
+
+func ftpDownloadAndHash(t *testing.T, ftp *goftp.FTP, filename string) string {
+	var hash string
+	readFunc := func(r io.Reader) error {
+		var hasher = sha256.New()
+		if _, err := io.Copy(hasher, r); err != nil {
+			return err
+		}
+
+		hash = hex.EncodeToString(hasher.Sum(nil))
+
+		return nil
+	}
+
+	if _, err := ftp.Retr(filename, readFunc); err != nil {
+		t.Fatal("Couldn't fetch file:", err)
+	}
+
+	return hash
+}
+
+func ftpDelete(t *testing.T, ftp *goftp.FTP, filename string) {
+	if err := ftp.Dele("file2.bin"); err != nil {
+		t.Fatal("Couldn't ftpDelete file", err)
+	}
+
+	if err := ftp.Dele("file2.bin"); err == nil {
+		t.Fatal("Should have had a problem deleting file2.bin")
+	}
+}
+
 func TestTransfer(t *testing.T) {
 	s := NewTestServer(true)
 	defer s.Stop()
@@ -29,89 +125,15 @@ func TestTransfer(t *testing.T) {
 	}
 
 	var hashUpload, hashDownload string
-	{ // We create a 20MB file and upload it
-		var file *os.File
-		var fileErr error
-		if file, fileErr = ioutil.TempFile("", "ftpserver"); fileErr != nil {
-			t.Fatal("Temporary creation error:", fileErr)
-		}
-
-		targetSize := 10 * 1024 * 1024
-
-		src := rand.New(rand.NewSource(0))
-		if _, err := io.CopyN(file, src, int64(targetSize)); err != nil {
-			t.Fatal("Couldn't copy:", err)
-		}
-
-		if _, err := file.Seek(0, 0); err != nil {
-			t.Fatal("Couldn't seek:", err)
-		}
-
-		hashser := sha256.New()
-		if _, err := io.Copy(hashser, file); err != nil {
-			t.Fatal("Couldn't hashUpload:", err)
-		}
-
-		hashUpload = hex.EncodeToString(hashser.Sum(nil))
-		if _, err := file.Seek(0, 0); err != nil {
-			t.Fatal("Couldn't seek:", err)
-		}
-
-		if err := ftp.Stor("file1.bin", file); err != nil {
-			t.Fatal("Couldn't upload bin:", err)
-		}
-
-		if size, err := ftp.Size("file1.bin"); err != nil {
-			t.Fatal("Couldn't get the size of file1.bin:", err)
-		} else {
-			if size != targetSize {
-				t.Fatalf("Size is %d instead of %d", size, targetSize)
-			}
-		}
-
-		if err := ftp.Rename("file1.bin", "file2.bin"); err != nil {
-			t.Fatal("Can't rename file:", err)
-		}
-
-		if stats, err := ftp.Stat("file2.bin"); err != nil {
-			// That's acceptable for now
-			t.Log("Couldn't stat file:", err)
-		} else {
-			found := false
-			for _, l := range stats {
-				if strings.HasSuffix(l, "file2.bin") {
-					found = true
-				}
-			}
-			if !found {
-				t.Fatal("STAT: Couldn't find file !")
-			}
-		}
+	{ // We create a 10MB file and ftpUpload it
+		file := createTemporaryFile(t, 10 * 1024 * 1024)
+		hashUpload = hashFile(t, file)
+		ftpUpload(t, ftp, file, "file.bin")
 	}
 
 	{ // We download the file we just uploaded
-		readFunc := func(r io.Reader) error {
-			var hasher = sha256.New()
-			if _, err := io.Copy(hasher, r); err != nil {
-				return err
-			}
-
-			hashDownload = hex.EncodeToString(hasher.Sum(nil))
-
-			return nil
-		}
-
-		if _, err := ftp.Retr("file2.bin", readFunc); err != nil {
-			t.Fatal("Couldn't fetch file:", err)
-		}
-
-		if err := ftp.Dele("file2.bin"); err != nil {
-			t.Fatal("Couldn't delete file", err)
-		}
-
-		if err := ftp.Dele("file2.bin"); err == nil {
-			t.Fatal("Should have had a problem deleting file2.bin")
-		}
+		hashDownload = ftpDownloadAndHash(t, ftp, "file.bin")
+		ftpDelete(t, ftp, "file.bin")
 	}
 
 	// We make sure the hashes of the two files match
