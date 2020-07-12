788111b0d897ef9342c9ca5a7790f4c8db2b6f67
Florent Clairambault <florent@habx.fr>
2020-07-12 21:12:34 +0200

    Various fixes to path the new linters constraints

--- fs/afos/afos.go
+++ fs/afos/afos.go
@@ -9,11 +9,14 @@ import (
 	"github.com/fclairamb/ftpserver/config/confpar"
 )
 
+// ErrMissingBasePath is triggered when the basePath property isn't specified
+var ErrMissingBasePath = errors.New("basePath must be specified")
+
 // LoadFs loads a file system from an access description
 func LoadFs(access *confpar.Access) (afero.Fs, error) {
 	basePath := access.Params["basePath"]
 	if basePath == "" {
-		return nil, errors.New("basePath must be specified")
+		return nil, ErrMissingBasePath
 	}
 
 	return afero.NewBasePathFs(afero.NewOsFs(), basePath), nil
