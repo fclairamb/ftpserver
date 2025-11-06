5279d5eff0ff9abbc341fe699b96775a9cd9f82b
Alexandr SARANDI <sarandi.alex@gmail.com>
2025-11-05 21:35:07 -0500

    fix(conf): added serialization tests

--- config/confpar/confpar_test.go
+++ config/confpar/confpar_test.go
@@ -1,10 +1,34 @@
 package confpar
 
 import (
+	"bytes"
 	"encoding/json"
 	"testing"
 	"time"
 )
+
+func TestDurationMarshalJSON(t *testing.T) {
+	for _, tt := range []struct {
+		input    Duration
+		expected []byte
+	}{
+		{Duration{0}, []byte(`"0s"`)},
+		{Duration{31 * time.Second}, []byte(`"31s"`)},
+		{Duration{5 * time.Minute}, []byte(`"5m0s"`)},
+		{Duration{time.Hour}, []byte(`"1h0m0s"`)},
+	} {
+
+		b, err := json.Marshal(&tt.input)
+
+		if err != nil {
+			t.Fatalf("json.Marshal(): %v", err)
+		}
+
+		if !bytes.Equal(b, tt.expected) {
+			t.Fatalf("have:%q want:%q", b, tt.expected)
+		}
+	}
+}
 
 func TestDurationUnmarshalJSON(t *testing.T) {
 	for _, tt := range []struct {
@@ -18,8 +42,8 @@ func TestDurationUnmarshalJSON(t *testing.T) {
 		{[]byte(`"0s"`), 0, false},
 		{[]byte(`"invalid"`), 0, true},
 	} {
-		val := &Duration{}
-		err := json.Unmarshal(tt.input, &val)
+		var d Duration
+		err := json.Unmarshal(tt.input, &d)
 
 		if err == nil && tt.wantErr {
 			t.Fatalf("expecting error for invalid input")
@@ -29,8 +53,8 @@ func TestDurationUnmarshalJSON(t *testing.T) {
 			t.Fatalf("json.Unmarshal(): %v", err)
 		}
 
-		if val.Duration != tt.expected {
-			t.Fatalf("have:%v want:%v", val.Duration, tt.expected)
+		if d.Duration != tt.expected {
+			t.Fatalf("have:%v want:%v", d.Duration, tt.expected)
 		}
 	}
 }
