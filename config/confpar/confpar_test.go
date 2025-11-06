d0001c834a41aca741c6d816a6fdea78b2256648
Alexandr SARANDI <sarandi.alex@gmail.com>
2025-11-05 20:34:49 -0500

    fix(conf): updated test error messages

--- config/confpar/confpar_test.go
+++ config/confpar/confpar_test.go
@@ -22,15 +22,15 @@ func TestDurationUnmarshalJSON(t *testing.T) {
 		err := json.Unmarshal(tt.input, &val)
 
 		if err == nil && tt.wantErr {
-			t.Fatalf("Duration{} UnmarshalJSON(): expecting error for invalid input")
+			t.Fatalf("expecting error for invalid input")
 		}
 
 		if err != nil && !tt.wantErr {
-			t.Fatalf("&Duration{} UnmarshalJSON(): %v", err)
+			t.Fatalf("json.Unmarshal(): %v", err)
 		}
 
 		if val.Duration != tt.expected {
-			t.Fatalf("Duration{} UnmarshalJSON(): have:%v want:%v", val.Duration, tt.expected)
+			t.Fatalf("have:%v want:%v", val.Duration, tt.expected)
 		}
 	}
 }
